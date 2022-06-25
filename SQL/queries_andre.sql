597185b89d690c8a90ba33e454247606c85c05f1
andre-sqr <andrebsqr@gmail.com>
2022-06-25 15:56:25 -0300

    add: primeira pergunta da query de andre

--- SQL/queries_andre.sql
+++ SQL/queries_andre.sql
@@ -1,6 +1,15 @@
-# Quais são as 10 categorias mais populares de jogos?
+# Quais são as 10 categorias mais populares de jogos na Steam?
 
-SELECT DISTINCT nome_categoria FROM steam_db.categorias;
-SELECT * FROM steam_db.jogos;
+SELECT DISTINCT
+    nome_categoria, COUNT(nome_categoria) AS contador
+FROM
+    steam_db.categorias
+GROUP BY nome_categoria
+ORDER BY contador DESC
+LIMIT 10;
 
-# Quais são as 10 categorias mais populares de jogos indie?
+# Quais são as 10 categorias mais populares de jogos indie na Steam?
+
+# Qual ano mais foi lançado jogos na Steam?
+
+# Qual ano mais foi lançado jogos indie na Steam?
