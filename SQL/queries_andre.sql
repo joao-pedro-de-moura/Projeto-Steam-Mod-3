37a347f2a21d25b3e4d340c638e2ab2b20efd68d
andre-sqr <andrebsqr@gmail.com>
2022-06-25 20:48:26 -0300

    add: quarta pergunta da query andre

--- SQL/queries_andre.sql
+++ SQL/queries_andre.sql
@@ -33,3 +33,13 @@ ORDER BY frequencia DESC
 LIMIT 5;
 
 # Qual ano mais foi lançado jogos indie na Steam?
+
+SELECT 
+    RIGHT(data_lancamento, 4) AS ano,
+    COUNT(RIGHT(data_lancamento, 4)) AS frequencia
+FROM
+    steam_db.jogos
+    WHERE steam_db.jogos.genero like "%indie%"
+GROUP BY ano
+ORDER BY frequencia DESC
+LIMIT 5;
