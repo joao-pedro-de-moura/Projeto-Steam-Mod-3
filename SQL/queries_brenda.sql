faf64d7646b61bc24b0eaada423807d39829d640
brendatondin <bentotbrenda@gmail.com>
2022-06-27 00:50:50 -0300

    pergunta sobre horas jogadas em jogos indie

--- SQL/queries_brenda.sql
+++ SQL/queries_brenda.sql
@@ -25,3 +25,11 @@ FROM
 GROUP BY nome
 ORDER BY avaliacoes_positivas DESC
 LIMIT 5;
+
+#Quantas horas foram jogadas no total em jogos indie na Steam? 
+SELECT 
+    SUM(jogos.tempo_de_jogo_medio)
+FROM
+    jogos
+WHERE
+    genero LIKE '%Indie%';
