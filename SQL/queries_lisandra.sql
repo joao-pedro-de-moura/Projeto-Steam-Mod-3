42ad4669fe0f87f1db3cdf6abade7c04ace94c44
lisandradecassia <lisandradecassia@gmail.com>
2022-06-28 16:29:12 -0300

    Conslta dos top dez jogos não indies.

--- SQL/queries_lisandra.sql
+++ SQL/queries_lisandra.sql
@@ -9,3 +9,20 @@ FROM
 GROUP BY nome_desenvolvedor
 ORDER BY quantidade DESC
 LIMIT 10;
+
+#dentre o top 10 desenvolvedores, quais deles sao indie?
+SELECT 
+    nome_desenvolvedor, quantidade
+FROM
+    (SELECT 
+        COUNT(desenvolvedores.nome_desenvolvedor) AS quantidade,
+            nome_desenvolvedor,
+            jogos.genero
+    FROM
+        desenvolvedores
+    INNER JOIN jogos ON desenvolvedores.appid = jogos.appid
+    GROUP BY nome_desenvolvedor
+    ORDER BY quantidade DESC
+    LIMIT 10) AS subquery
+WHERE
+    genero LIKE '%Indie%';
