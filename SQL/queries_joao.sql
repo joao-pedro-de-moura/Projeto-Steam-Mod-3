6df78c8a77696e81f2782e1662d99c19ed8610ea
brendatondin <bentotbrenda@gmail.com>
2022-06-28 01:13:04 -0300

    Joao Pedro de Moura - Quantos jogos indie desenvovidos são comaptíveis com todos os sistemas operacionais?

--- SQL/queries_joao.sql
+++ SQL/queries_joao.sql
@@ -13,3 +13,14 @@ FROM
     jogos
 WHERE
     NOT genero LIKE '%indie%';
+    
+#Quantos jogos indie desenvovidos são comaptíveis com todos os sistemas operacionais?
+SELECT 
+    COUNT(jogos.appid)
+FROM
+    jogos
+        INNER JOIN
+    plataformas ON jogos.appid = plataformas.appid
+WHERE
+    nome_plataforma LIKE '%windows_mac_linux%'
+        AND genero LIKE '%indie%';    
