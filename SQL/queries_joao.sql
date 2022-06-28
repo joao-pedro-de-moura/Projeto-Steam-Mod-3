d52a26070e4242a4ad7518f00954ee557065e6ba
brendatondin <bentotbrenda@gmail.com>
2022-06-28 01:08:19 -0300

    Joao Pedro de Moura - media de preço de jogos não indie na Steam

--- SQL/queries_joao.sql
+++ SQL/queries_joao.sql
@@ -5,3 +5,11 @@ FROM
     jogos
 WHERE
     genero LIKE '%indie%';
+    
+#Qual a média de preço dos jogos nao indie na Steam?
+SELECT 
+    AVG(preco_jogo)
+FROM
+    jogos
+WHERE
+    NOT genero LIKE '%indie%';
